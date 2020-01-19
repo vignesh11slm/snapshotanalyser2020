770fe923a37b4e06916357f6435784a7ed9cfc94
vignesh <Vignesh11slm@hotmail.com>
2020-01-19 03:42:32 +0000

    Added cick functionality to shotty.py

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,10 +1,15 @@
 import boto3
+import click
+
 session = boto3.Session()
 ec2 = session.resource('ec2')
 
+@click.command()
 def list_instance():
+    "List all ec2 instances"
     for i in ec2.instances.all():
-    	print(i)
-
+        #print(i.id + " " i.instance_type)
+        print(','.join((i.id,i.instance_type,i.state['Name'],i.public_dns_name)))
 if __name__ == '__main__':
     list_instance()
+
